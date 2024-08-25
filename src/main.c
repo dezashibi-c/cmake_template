6853385391ba2d669800a4cef2910b6bbe13078c
Navid Dezashibi <dezashibi@gmail.com>
2024-08-25 11:31:29 +0400

    first commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "math_utils.h"
+#include <stdio.h>
+
+int main()
+{
+    int a = 5, b = 3;
+    printf("Sum: %d\n", add(a, b));
+    printf("Difference: %d\n", subtract(a, b));
+
+    puts("Hey There");
+
+    return 0;
+}
---
+++ tests/test_math_utils.c
@@ -0,0 +1,18 @@
+#define CLOVE_SUITE_NAME MathUtilTests
+
+#include "clove-unit.h"
+#include "math_utils.h"
+
+CLOVE_TEST(Addition)
+{
+    int result = add(2, 3);
+
+    CLOVE_INT_EQ(result, 5);
+}
+
+CLOVE_TEST(Subtraction)
+{
+    int result = subtract(5, 3);
+
+    CLOVE_INT_EQ(result, 2);
+}
